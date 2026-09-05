337d453709a57b9678e792be2b1d432a3d988846
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-09-05 22:54:52 +0530

    operators

--- java.js
+++ java.js
@@ -1,16 +1,25 @@
-fullname = "Aditya singh";
-console.log(fullname);
+// fullname = "Aditya singh";
+// console.log(fullname);
 
-age="12";
-console.log(age)
-//Operators 
-let a = 5;
-let b = 6;
-console.log("a + b = ",a+b);
-console.log("a - b = ",a-b);
-console.log("a * b = ",a*b);
-console.log("a / b = ",a/b);
-console.log("a % b = ",a%b);
+// age="12";
+// console.log(age)
+// //Operators 
+// let a = 5;
+// let b = 6;
+// console.log("a + b = ",a+b);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// console.log("a % b = ",a%b);
+let number = prompt("Enter the number");
+console.log(number)
+if(number%5==0){
+    console.log("number is divisible by 5");
+
+}
+else{
+    console.log("numbver is not divisible by 5");
+}
 
 
 
