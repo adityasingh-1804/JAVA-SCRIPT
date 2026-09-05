7131623772b815aa5e90c7277d325a274bdeccd8
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-09-05 22:18:36 +0530

    operators

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+fullname = "Aditya singh";
+console.log(fullname);
+
+age="12";
+console.log(age)
+//Operators 
+let a = 5;
+let b = 6;
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a % b = ",a%b);
+
+
+
+
+
+
